f2c86671afeb9a4eac869cb8fbd63c0819162740
Cronus314 <93542597+Cronus314@users.noreply.github.com>
2024-04-08 23:24:05 -0700

    Style customization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,35 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_material'
 
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Wild Raccoons’s FRC Documentation',
+
+    # Set you GA account ID to enable tracking
+    #'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://wildraccoons8891.readthedocs.io/en/latest/',
+
+    # Set the color and the accent color
+    'color_primary': 'purple',
+    'color_accent': 'light-purple',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/wildraccoons/Documentation',
+    'repo_name': 'Documentation',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
