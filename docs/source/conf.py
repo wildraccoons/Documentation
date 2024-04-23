447a4a1b121d9f53194961fafd509087e6312d0f
Cronus314 <93542597+Cronus314@users.noreply.github.com>
2024-04-22 23:36:49 -0700

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ html_theme_options = {
     # Set the name of the project to appear in the navigation.
     'nav_title': 'Wild Raccoons’s FRC Documentation',
 
-    'theme_color':'9240ad'
+    'theme_color': 9240ad
 
     
 }
