aadf34d76917201a7db2a56eff5573c3893655c9
Cronus314 <93542597+Cronus314@users.noreply.github.com>
2024-05-11 15:32:02 -0700

    added commas

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,9 +36,9 @@ html_theme_options = {
     # Set the name of the project to appear in the navigation.
     'nav_title': 'Wild Raccoons’s FRC Documentation',
 
-    'theme_color': '9240ad'
+    'theme_color': '9240ad',
 
-    'color_primary': 'purple'
+    'color_primary': 'purple',
 
     'color_accent': 'green'
 
